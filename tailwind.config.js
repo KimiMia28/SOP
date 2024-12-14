8f8850379195fe73dcff59d124622234cf062952
KimiMia28 <Julie05td@gmail.com>
2024-12-14 17:31:01 +0100

    Organiserede mapper og tilføjede iframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./NOVAS/**/*.{html,js}"],
+  content: ["./NOVAS/**/*.{html,js}", "./NOVAS/Frames/**/*.{html,js}"],
   theme: {
     fontFamily: {
       "display":["rufina"]
